794b784e98f8c809c26bc70d043bcda44931e879
prasad <ncprasadmadusanka@gmail.com>
2020-10-18 15:45:49 +0530

    Updates

---
+++ precache-manifest.49346d3fdd79866e339a2dc7c905e2b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bcf0833f032ce2c57b70f4243e0a20",
+    "url": "/React_Context_Hooks_Reading_App/index.html"
+  },
+  {
+    "revision": "708145ddbe9c42b79569",
+    "url": "/React_Context_Hooks_Reading_App/static/css/main.70f2a179.chunk.css"
+  },
+  {
+    "revision": "4b45bc14e0c9c9c3f370",
+    "url": "/React_Context_Hooks_Reading_App/static/js/2.0a212094.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_Context_Hooks_Reading_App/static/js/2.0a212094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708145ddbe9c42b79569",
+    "url": "/React_Context_Hooks_Reading_App/static/js/main.f8140274.chunk.js"
+  },
+  {
+    "revision": "cedcb2c9ebf605dd7640",
+    "url": "/React_Context_Hooks_Reading_App/static/js/runtime-main.feac99d3.js"
+  }
+]);
